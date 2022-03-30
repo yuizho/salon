5ce7d66dcbd89a375fd1d194f3f55ff2aed2deb4
yuizho <yuizho3@gmail.com>
2022-03-30 22:34:43 +0900

    modify room-stack.ts to add RoomAPI query config

--- room/lib/room-stack.ts
+++ room/lib/room-stack.ts
@@ -1,24 +1,57 @@
 import * as cdk from "@aws-cdk/core";
+import * as appsync from "@aws-cdk/aws-appsync";
 import * as db from "@aws-cdk/aws-dynamodb";
 
 export class RoomStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // AppSync
+    const api = new appsync.GraphqlApi(this, "RoomAPI", {
+      name: "RoomAPI",
+      schema: appsync.Schema.fromAsset("appsync/schema.graphql"),
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: appsync.AuthorizationType.API_KEY,
+          apiKeyConfig: {
+            name: "default",
+            description: "default auth mode",
+            expires: cdk.Expiration.after(cdk.Duration.days(365)),
+          },
+        },
+      },
+    });
+
     // DynamoDB
     // https://docs.aws.amazon.com/cdk/api/v1/docs/@aws-cdk_aws-dynamodb.Table.html
-    const operation_table = new db.Table(this, "OperationTable", {
+    const operationTable = new db.Table(this, "OperationTable", {
       tableName: "operation",
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       billingMode: db.BillingMode.PAY_PER_REQUEST,
       partitionKey: { name: "room_id", type: db.AttributeType.STRING },
     });
-    const room_table = new db.Table(this, "RoomTable", {
+    const roomTable = new db.Table(this, "RoomTable", {
       tableName: "room",
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       billingMode: db.BillingMode.PAY_PER_REQUEST,
       partitionKey: { name: "room_id", type: db.AttributeType.STRING },
       sortKey: { name: "user_id", type: db.AttributeType.STRING },
+    });
+
+    // Set up table as a Datasource and grant access
+    const roomDataSource = api.addDynamoDbDataSource(
+      "roomDataSource",
+      roomTable
+    );
+
+    // Define resolvers
+    roomDataSource.createResolver({
+      typeName: "Query",
+      fieldName: "getRoom",
+      requestMappingTemplate: appsync.MappingTemplate.fromFile(
+        "appsync/resolvers/Query.getRoom.req.vtl"
+      ),
+      responseMappingTemplate: appsync.MappingTemplate.dynamoDbResultItem(),
     });
 
     // Stack Ouputs
