4ed2336d53677d3eddbc4ae6a74fbd271d585a25
yuizho <yuizho3@gmail.com>
2022-04-24 17:17:39 +0900

    add rmu lambda function tests

---
+++ room/lambda/room-rmu/service/room_service_test.go
@@ -0,0 +1,59 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/yuizho/salon/room/lambda/room-rmu/model"
+)
+
+type TestRepos struct {
+}
+
+func (repos *TestRepos) Save(context context.Context, room *model.Room) error {
+	return nil
+}
+func (repos *TestRepos) FindActiveUsers(context context.Context, roomId string) ([]model.Room, error) {
+	return []model.Room{
+		{
+			RoomId:     "1",
+			UserId:     "1",
+			Status:     model.Status("CHOOSING"),
+			OperatedAt: "2022-10-10T13:50:40Z",
+		},
+		{
+			RoomId:     "1",
+			UserId:     "2",
+			Status:     model.Status("CHOOSING"),
+			OperatedAt: "2022-10-10T13:50:40Z",
+		},
+	}, nil
+}
+func (repos *TestRepos) UpdateActiveUserStatus(context context.Context, room *model.Room, status model.Status) error {
+	// part of update is failed
+	if room.UserId == "2" {
+		return fmt.Errorf("some error")
+	}
+	return nil
+}
+
+func TestRefleshPoker(t *testing.T) {
+	input := make(map[string]events.DynamoDBAttributeValue)
+	input["event_id"] = events.NewStringAttribute("9999")
+	input["room_id"] = events.NewStringAttribute("1")
+	input["user_id"] = events.NewStringAttribute("2")
+	input["op_type"] = events.NewStringAttribute("REFRESH_TABLE")
+	input["picked_card"] = events.NewNullAttribute()
+	input["operated_at"] = events.NewStringAttribute("2022-10-10T13:50:40Z")
+
+	err := NewRoomService(&TestRepos{}).SaveRoom(
+		context.TODO(),
+		input,
+	)
+
+	if err != nil {
+		t.Fatalf("failed to update user status: %v", err)
+	}
+}
