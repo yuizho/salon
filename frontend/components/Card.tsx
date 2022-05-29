34954633a0cb100d2195f8d068a9c35676164690
yuizho <yuizho3@gmail.com>
2022-05-29 20:09:37 +0900

    introduce tailwind css

--- frontend/components/Card.tsx
+++ frontend/components/Card.tsx
@@ -1,15 +1,49 @@
 import { FC } from 'react';
-import styles from './Card.module.css';
+import pattern from '../public/intersecting-circles.svg';
 
 type Props = {
   value: string;
+  isShown?: boolean;
+  isChoosable?: boolean;
 };
 
 // TODO: this is sample module
-const Card: FC<Props> = ({ value }) => (
-  <div key={value} className={styles.card}>
+const Card: FC<Props> = ({ value, isShown = true, isChoosable = true }) => (isShown ? shown(value, isChoosable) : hidden(value, isChoosable));
+
+const baseClassNames = `
+flex
+items-center justify-center
+w-16 h-20
+rounded border border-black/50
+shadow-xl
+`;
+
+const chooableClassNames = `
+hover:animate-bounce hover:cursor-pointer
+`;
+
+const shown = (value: string, isChoosable: boolean) => (
+  <div
+    key={value}
+    className={`
+      ${baseClassNames}
+      ${isChoosable ? chooableClassNames : ''} 
+      font-bold text-2xl
+    `}
+  >
     {value}
   </div>
+);
+
+const hidden = (value: string, isChoosable: boolean) => (
+  <div
+    key={value}
+    className={`
+    ${baseClassNames}
+    ${isChoosable ? chooableClassNames : ''} 
+    `}
+    style={{ backgroundImage: `url(${pattern})` }}
+  />
 );
 
 export default Card;
