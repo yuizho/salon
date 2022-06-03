02b769e88a6401411311861e87e898679c4229fb
yuizho <yuizho3@gmail.com>
2022-06-03 22:49:12 +0900

    add ModalDialog component

---
+++ frontend/components/ModalDialog.stories.tsx
@@ -0,0 +1,29 @@
+import { Story, Meta } from '@storybook/react/types-6-0';
+import { useRef } from 'react';
+import { Component } from './ModelDialog';
+
+export default {
+  title: 'ModalDialog',
+  component: Component,
+} as Meta;
+
+const Template: Story = ({ title, content, open, setOpen, onClickOK }) => {
+  return (
+    <Component
+      title={title}
+      content={content}
+      open={open}
+      setOpen={setOpen}
+      onClickOK={onClickOK}
+    />
+  );
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  open: true,
+  setOpen: (b: boolean) => console.log(b),
+  title: 'タイトルです！！',
+  content: '選択したユーザを部屋からキックします！よろしいですか？',
+  onClickOK: () => alert('OK button is clicked!!!'),
+};
