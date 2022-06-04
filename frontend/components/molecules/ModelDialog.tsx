f8d3d32b7750f0cc3278b0dd26a0e52b264f46b4
yuizho <yuizho3@gmail.com>
2022-06-04 20:28:33 +0900

    modify ModalDialog component

--- frontend/components/molecules/ModelDialog.tsx
+++ frontend/components/molecules/ModelDialog.tsx
@@ -1,5 +1,6 @@
 import { FC, Fragment, useState } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
+import Button from '../atoms/Button';
 
 type Props = {
   message: string;
@@ -45,31 +46,20 @@ export const Component: FC<ComponentProps> = ({
             <Dialog.Panel className="relative bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full">
               <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                 <div className="sm:flex sm:items-start">
-                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                    <div className="mt-2">
-                      <p className="text-sm text-gray-500">{message}</p>
-                    </div>
+                  <div className="py-3 text-center sm:text-left">
+                    <p className="text-sm text-slate-600">{message}</p>
                   </div>
                 </div>
               </div>
-              <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                <button
-                  type="button"
-                  className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
+              <div className="px-4 py-3 flex gap-2 flex-row-reverse">
+                <Button
+                  text="OK"
                   onClick={() => {
                     onClickOK();
                     setOpen(false);
                   }}
-                >
-                  OK
-                </button>
-                <button
-                  type="button"
-                  className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-                  onClick={() => setOpen(false)}
-                >
-                  Cancel
-                </button>
+                />
+                <Button text="Cancel" onClick={() => setOpen(false)} />
               </div>
             </Dialog.Panel>
           </Transition.Child>
