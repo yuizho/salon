e2284dca2202dde06f08e0ca763d432bee3e4479
yuizho <yuizho3@gmail.com>
2022-06-05 21:29:47 +0900

    introduce amplify

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Amplify } from 'aws-amplify';
+import { RecoilRoot } from 'recoil';
+
+Amplify.configure({
+  aws_appsync_region: 'ap-northeast-1',
+  aws_appsync_graphqlEndpoint: process.env.NEXT_PUBLIC_APPSYNC_URL,
+  aws_appsync_authenticationType: 'API_KEY',
+  aws_appsync_apiKey: process.env.NEXT_PUBLIC_APPSYNC_API_KEY,
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
+  );
 }
 
-export default MyApp
+export default MyApp;
