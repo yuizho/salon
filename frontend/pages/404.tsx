fdf66695ab7d7c9b216238a1c10c024c0b749555
yuizho <yuizho3@gmail.com>
2022-06-13 23:24:46 +0900

    modify 404 page

--- frontend/pages/404.tsx
+++ frontend/pages/404.tsx
@@ -3,7 +3,7 @@ import Frame from '../components/templates/Frame';
 
 const NotFound: FC = (): JSX.Element => (
   <Frame>
-    <div className="flex flex-col space-y-6 border rounded p-8">
+    <div className="flex flex-col space-y-6 border rounded p-8 text-slate-600">
       指定された部屋はクローズされました
     </div>
   </Frame>
