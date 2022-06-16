64429a4697577a97fa30535923577d9e66b0888a
yuizho <yuizho3@gmail.com>
2022-06-16 22:52:42 +0900

    modify top page
    to fix explanation

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -5,8 +5,8 @@ import Frame from '../components/templates/Frame';
 const Home: FC = (): JSX.Element => (
   <Frame>
     <main className="flex flex-col gap-5">
-      <p className="text-slate-700 text-lg font-semibold">
-        Salonはログイン不要の無料オンラインプランニングポーカーWebアプリです
+      <p className="text-slate-700 text-lg font-semibold px-1">
+        Salonはログイン不要の無料Webプランニングポーカーサービスです
       </p>
       <RoomForm />
     </main>
