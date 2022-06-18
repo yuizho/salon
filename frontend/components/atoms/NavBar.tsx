a52ac51dcb4316974bb0eb9d08adba61aaebbd01
yuizho <yuizho3@gmail.com>
2022-06-18 15:28:16 +0900

    modify Header to add GitHub icon

--- frontend/components/atoms/NavBar.tsx
+++ frontend/components/atoms/NavBar.tsx
@@ -1,13 +1,22 @@
+import Link from 'next/link';
 import { FC } from 'react';
+import { BsGithub } from 'react-icons/bs';
 
 export const Component: FC = () => (
-  <nav className="flex ustify-between flex-wrap py-4 px-6 border-b">
+  <nav className="flex justify-between flex-wrap py-4 px-6 border-b">
     <div className="flex items-center flex-shrink-0 text-slate-800">
       {/* use a tag intentionally here to run leave process by leave hooks */}
       {/* eslint-disable @next/next/no-html-link-for-pages */}
       <a className="font-bold text-2xl tracking-tight" href="/">
         Salon
       </a>
+    </div>
+    <div className="flex items-center flex-shrink-0 text-slate-600 text-xl">
+      <Link href="https://github.com/yuizho/salon">
+        <a target="_blank" rel="noopener noreferrer">
+          <BsGithub />
+        </a>
+      </Link>
     </div>
   </nav>
 );
