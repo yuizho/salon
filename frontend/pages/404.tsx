1539dddfb3aeaccffc563a94e183f2b064673787
yuizho <yuizho3@gmail.com>
2022-06-18 17:00:27 +0900

    modify 404 page explanation

--- frontend/pages/404.tsx
+++ frontend/pages/404.tsx
@@ -1,10 +1,15 @@
 import { FC } from 'react';
+import Link from '../components/atoms/Link';
 import Frame from '../components/templates/Frame';
 
 const NotFound: FC = (): JSX.Element => (
   <Frame>
-    <div className="flex flex-col space-y-6 border rounded p-8 text-slate-600">
-      指定された部屋はクローズされました
+    <div className="space-y-6 border rounded p-8 text-slate-600">
+      この部屋は有効期限を過ぎたのでクローズされました。新たにプランニングポーカーを始めるには{' '}
+      <Link href="/" className="underline">
+        トップページ
+      </Link>{' '}
+      から新しい部屋を作成してください。
     </div>
   </Frame>
 );
