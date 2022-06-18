e4c33223136a6ac5ba8e25e118bf73bf9fb99293
yuizho <yuizho3@gmail.com>
2022-06-18 11:14:10 +0900

    modify NavBar link
    to execute leave process when the link is pushed

--- frontend/components/atoms/NavBar.tsx
+++ frontend/components/atoms/NavBar.tsx
@@ -1,12 +1,13 @@
-import Link from 'next/link';
 import { FC } from 'react';
 
 export const Component: FC = () => (
   <nav className="flex ustify-between flex-wrap py-4 px-6 border-b">
     <div className="flex items-center flex-shrink-0 text-slate-800">
-      <Link href="/">
-        <a className="font-bold text-2xl tracking-tight">Salon</a>
-      </Link>
+      {/* use a tag intentionally here to run leave process by leave hooks */}
+      {/* eslint-disable @next/next/no-html-link-for-pages */}
+      <a className="font-bold text-2xl tracking-tight" href="/">
+        Salon
+      </a>
     </div>
   </nav>
 );
