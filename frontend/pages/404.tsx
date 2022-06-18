b15fcac3c1d5872edb468b0e8533c37605c5d22a
yuizho <yuizho3@gmail.com>
2022-06-18 20:47:37 +0900

    modify link of 404 page

--- frontend/pages/404.tsx
+++ frontend/pages/404.tsx
@@ -1,14 +1,15 @@
 import { FC } from 'react';
-import Link from '../components/atoms/Link';
 import Frame from '../components/templates/Frame';
 
 const NotFound: FC = (): JSX.Element => (
   <Frame>
     <div className="space-y-6 border rounded p-8 text-slate-600">
       この部屋は有効期限を過ぎたのでクローズされました。新たにプランニングポーカーを始めるには{' '}
-      <Link href="/" className="underline">
+      {/* use a tag intentionally here to run leave process by leave hooks */}
+      {/* eslint-disable @next/next/no-html-link-for-pages */}
+      <a href="/" className="underline">
         トップページ
-      </Link>{' '}
+      </a>{' '}
       から新しい部屋を作成してください。
     </div>
   </Frame>
