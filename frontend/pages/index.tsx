feff67539d31ea6fc6daba1caad81722aff972d4
yuizho <yuizho3@gmail.com>
2022-06-18 17:28:31 +0900

    modify index page to add explination of planning poker

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -7,24 +7,35 @@ const Home: FC = (): JSX.Element => (
   <Frame>
     <main className="flex flex-col gap-5">
       <RoomForm />
-
-      <h2 className="text-slate-700 font-semibold px-1">免責事項</h2>
-      <div className="text-slate-600 text-sm px-6">
-        <ul className="list-disc">
-          <li>
-            当サービスは{' '}
-            <Link href="https://github.com/yuizho" newWindow className="underline">
-              yuizho
-            </Link>{' '}
-            が個人で運営するサービスです。サービスの維持が難しくなった場合には予告なくサービスを終了するかもしれません。
-          </li>
-          <li>
-            できる範囲でサービスの維持に努めますが、当サービスで検出された不具合やそれが原因で発生した損害などについては一切責任を負いかねます。
-          </li>
-          <li>
-            当サービスは、サービスの改善に役立てる目的で Googleアナリティクスを利用しています。
-          </li>
-        </ul>
+      <div>
+        <h2 className="text-slate-700 font-semibold">プランニングポーカーとは</h2>
+        <Link
+          href="https://en.wikipedia.org/wiki/Planning_poker"
+          newWindow
+          className="text-slate-600 text-sm px-1 underline"
+        >
+          https://en.wikipedia.org/wiki/Planning_poker
+        </Link>{' '}
+      </div>
+      <div>
+        <h2 className="text-slate-700 font-semibold px-1">免責事項</h2>
+        <div className="text-slate-600 text-sm px-6">
+          <ul className="list-disc">
+            <li>
+              当サービスは{' '}
+              <Link href="https://github.com/yuizho" newWindow className="underline">
+                yuizho
+              </Link>{' '}
+              が個人で運営するサービスです。サービスの維持が難しくなった場合には予告なくサービスを終了するかもしれません。
+            </li>
+            <li>
+              できる範囲でサービスの維持に努めますが、当サービスで検出された不具合やそれが原因で発生した損害などについては一切責任を負いかねます。
+            </li>
+            <li>
+              当サービスは、サービスの改善に役立てる目的で Googleアナリティクスを利用しています。
+            </li>
+          </ul>
+        </div>
       </div>
     </main>
   </Frame>
