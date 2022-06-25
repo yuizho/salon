96f9a08ccd1395f9f5b98665ffd8babdbf65f1ed
yuizho <yuizho3@gmail.com>
2022-06-25 20:41:00 +0900

    modify index page to add seo tags

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,38 +1,51 @@
+import Head from 'next/head';
 import { FC } from 'react';
 import Link from '../components/atoms/Link';
 import RoomForm from '../components/organisms/RoomForm';
 import Frame from '../components/templates/Frame';
 
 const Home: FC = (): JSX.Element => (
-  <Frame>
-    <main className="flex flex-col space-y-6">
-      <RoomForm />
-      <div className="flex flex-col space-y-3">
-        <h2 className="text-slate-700 font-semibold px-1">免責事項</h2>
-        <div className="text-slate-600 text-sm px-6">
-          <ul className="list-disc">
-            <li>
-              当サービスは{' '}
-              <Link href="https://github.com/yuizho" newWindow className="underline">
-                yuizho
-              </Link>{' '}
-              が個人で運営するサービスです。サービスの維持が難しくなった場合には予告なくサービスを終了するかもしれません。
-            </li>
-            <li>
-              できる範囲でサービスの維持に努めますが、当サービスで検出された不具合やそれが原因で発生した損害などについては一切責任を負いかねます。
-            </li>
-            <li>
-              当サービスは、サービスの改善に役立てる目的で{' '}
-              <Link href="https://sentry.io" newWindow className="underline">
-                Sentry
-              </Link>{' '}
-              を利用しています。
-            </li>
-          </ul>
+  <>
+    <Head>
+      <title>Salon</title>
+      <meta name="og:title" content="Salon" />
+      <meta property="og:site_name" content="Salon" />
+      <meta
+        name="og:description"
+        content="Salonはログイン不要の無料Webプランニングポーカーサービスです"
+      />
+    </Head>
+
+    <Frame>
+      <main className="flex flex-col space-y-6">
+        <RoomForm />
+        <div className="flex flex-col space-y-3">
+          <h2 className="text-slate-700 font-semibold px-1">免責事項</h2>
+          <div className="text-slate-600 text-sm px-6">
+            <ul className="list-disc">
+              <li>
+                当サービスは{' '}
+                <Link href="https://github.com/yuizho" newWindow className="underline">
+                  yuizho
+                </Link>{' '}
+                が個人で運営するサービスです。サービスの維持が難しくなった場合には予告なくサービスを終了するかもしれません。
+              </li>
+              <li>
+                できる範囲でサービスの維持に努めますが、当サービスで検出された不具合やそれが原因で発生した損害などについては一切責任を負いかねます。
+              </li>
+              <li>
+                当サービスは、サービスの改善に役立てる目的で{' '}
+                <Link href="https://sentry.io" newWindow className="underline">
+                  Sentry
+                </Link>{' '}
+                を利用しています。
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </main>
-  </Frame>
+      </main>
+    </Frame>
+  </>
 );
 
 export default Home;
