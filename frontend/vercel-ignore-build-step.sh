6990834faf824efe02fcdf45beee5427631f93ba
yuizho <yuizho3@gmail.com>
2022-06-26 10:37:40 +0900

    add vercel-ignore-build-step.sh
    only deploy develop branch to preview env

---
+++ frontend/vercel-ignore-build-step.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Reference
+# https://zenn.dev/catnose99/articles/b37104fc7ef214
+
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+
+if [[ "$VERCEL_GIT_COMMIT_REF" == "develop" ]] ; then
+  # Proceed with the build
+  echo "✅ - Build can proceed"
+  exit 1;
+
+else
+  # Don't build
+  echo "🛑 - Build cancelled"
+  exit 0;
+fi
