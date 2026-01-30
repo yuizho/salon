8eed12d247cf13ed08aaf4bcc551b5988842b2b0
yuizho <yuizho3@gmail.com>
2026-01-30 16:16:38 +0000

    fix: serve public assets in Storybook

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -5,6 +5,7 @@ module.exports = {
     '../components/**/*.stories.mdx',
     '../components/**/*.stories.@(js|jsx|ts|tsx)',
   ],
+  staticDirs: ['../public'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
