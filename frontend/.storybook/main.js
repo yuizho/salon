53afbb821731c4e77ded18e55ceca5220925ad5f
yuizho <yuizho3@gmail.com>
2026-01-30 15:48:06 +0000

    chore: migrate Storybook to Vite

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -16,43 +16,17 @@ module.exports = {
     },
   ],
   framework: {
-    name: '@storybook/react-webpack5',
+    name: '@storybook/react-vite',
     options: {},
   },
   docs: {
     autodocs: 'tag',
   },
-  webpackFinal: async (config) => {
+  viteFinal: async (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
       'next/config': path.join(__dirname, './next-config-mock.js'),
     };
-
-    config.resolve.fallback = {
-        ...config.resolve.fallback,
-        zlib: false,
-        fs: false,
-        stream: false,
-        os: false,
-    };
-
-    config.module.rules.push({
-      test: /\.(js|jsx|ts|tsx)$/,
-      exclude: /node_modules/,
-      use: [
-        {
-          loader: require.resolve('babel-loader'),
-          options: {
-            presets: [
-              ['@babel/preset-react', { runtime: 'automatic' }],
-              '@babel/preset-typescript',
-              '@babel/preset-env',
-            ],
-          },
-        },
-      ],
-    });
-
     return config;
   },
 };
